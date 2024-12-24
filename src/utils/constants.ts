5c632f98d2ccb75e6b6d5fc4077f056367299aad
Ezekwu <ezekwujerry@gmail.com>
2024-12-24 06:22:09 +0100

    ..

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,3 +4,29 @@ export const itemVariants = [
   { label: "Green", value: "green" },
   {label: 'NIL', value: 'nil'}
 ]
+
+export const paymentTerms= [
+  { label: "Net 30", value: "net_30" },
+  { label: "Net 15", value: "net_15" },
+  { label: "Net 60", value: "net_60" },
+  { label: "Due on Receipt", value: "due_on_receipt" },
+]
+
+export const deliverySchedule= [
+  { label: "Immediate delivery", value: "immediate" },
+  { label: "Standard delivery", value: "standard" },
+  { label: "Scheduled delivery", value: "scheduled" },
+]
+
+export const shippingMethods= [
+  { label: "Courier Services", value: "courier" },
+  { label: "Freight", value: "freight" },
+  { label: "Postal Service", value: "postal_service" },
+  { label: "Pick Up", value: "pickup" },
+]
+
+export const leadTime = {
+  value: 10,
+  unit: "days", 
+}
+
